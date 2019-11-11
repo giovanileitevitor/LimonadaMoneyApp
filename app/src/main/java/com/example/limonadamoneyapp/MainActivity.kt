c938f9df85328308b94cd85ea8daf6e27fdae5cb
giovani leite vitor <giovanileitevitor@gmail.com>
2019-11-10 23:34:29 -0300

    Adicionado XML das telas Consultar Gastos e Apontar Gastos

--- app/src/main/java/com/example/limonadamoneyapp/MainActivity.kt
+++ app/src/main/java/com/example/limonadamoneyapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.limonadamoneyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(
+            this, Navigation.findNavController(this, R.id.navHost)
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navHost).navigateUp()
     }
 }
